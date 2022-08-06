5603fef798b8c795ef2cc5801a11fb4f6d149399
dimden <firefox78888@gmail.com>
2022-08-06 21:38:38 +0300

    Update dimdenhotel.plugin.js

--- dimdenhotel.plugin.js
+++ dimdenhotel.plugin.js
@@ -1,6 +1,6 @@
 /**
  * @name dimdensHotelPlugin
- * @version 0.0.5
+ * @version 0.0.6
  * @website https://dimden.dev
  */
 
@@ -38,6 +38,49 @@ class dimdensHotelPlugin {
                 }
             }
         }, 1000);
+    }
+    getGradientColor(startColor, endColor, percent) {
+        // strip the leading # if it's there
+        startColor = startColor.replace(/^\s*#|\s*$/g, '');
+        endColor = endColor.replace(/^\s*#|\s*$/g, '');
+    
+        // convert 3 char codes --> 6, e.g. `E0F` --> `EE00FF`
+        if (startColor.length === 3) {
+          startColor = startColor.replace(/(.)/g, '$1$1');
+        }
+    
+        if (endColor.length === 3) {
+          endColor = endColor.replace(/(.)/g, '$1$1');
+        }
+    
+        // get colors
+        const startRed = parseInt(startColor.substr(0, 2), 16),
+          startGreen = parseInt(startColor.substr(2, 2), 16),
+          startBlue = parseInt(startColor.substr(4, 2), 16);
+    
+        const endRed = parseInt(endColor.substr(0, 2), 16),
+          endGreen = parseInt(endColor.substr(2, 2), 16),
+          endBlue = parseInt(endColor.substr(4, 2), 16);
+    
+        // calculate new color
+        let diffRed = endRed - startRed;
+        let diffGreen = endGreen - startGreen;
+        let diffBlue = endBlue - startBlue;
+    
+        diffRed = ((diffRed * percent) + startRed);
+        diffGreen = ((diffGreen * percent) + startGreen);
+        diffBlue = ((diffBlue * percent) + startBlue);
+    
+        let diffRedStr = diffRed.toString(16).split('.')[0];
+        let diffGreenStr = diffGreen.toString(16).split('.')[0];
+        let diffBlueStr = diffBlue.toString(16).split('.')[0];
+    
+        // ensure 2 digits by color
+        if (diffRedStr.length === 1) diffRedStr = '0' + diffRedStr;
+        if (diffGreenStr.length === 1) diffGreenStr = '0' + diffGreenStr;
+        if (diffBlueStr.length === 1) diffBlueStr = '0' + diffBlueStr;
+    
+        return '#' + diffRedStr + diffGreenStr + diffBlueStr;
     }
     async stop() {
         if (this.messageObserver) this.messageObserver.disconnect();
@@ -108,6 +151,9 @@ class dimdensHotelPlugin {
             let notToday = currentDate.getTime() - date.getTime() > 8.64e+7 || currentDate.getDay() !== date.getDay();
             if(notToday) time.parentElement.classList.add('hotel-not-today');
             time.innerText = `${notToday ? `${this.pad(date.getMonth()+1)}${this.pad(date.getDate())} ` : ''}${this.pad(date.getHours())}${this.pad(date.getMinutes())}`;
+            let dayminute = date.getHours()*60+date.getMinutes();
+            if(dayminute < 1440/2) time.style.color = this.getGradientColor("#0000ff", "#ffff00", dayminute/(1440/2));
+            else time.style.color = this.getGradientColor("#0000ff", "#ffff00", (1440-dayminute)/(1440/2));
         }
         let idSpan = document.createElement('span');
         idSpan.className = 'hotel-msg-userid';
